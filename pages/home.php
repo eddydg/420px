b4cdad1f78ca7f0e5b5375b66538f6c1a526e455
MeltedPenguin <diablo22007@gmail.com>
2016-04-25 15:51:37 +0200

    Show images in db on home page

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,10 @@
 <div id="content">
-    hello
+    <?php if ($Auth->isLogged()): ?>
+        <?php foreach ($ImageManager->getImages() as $image): ?>
+            <?php var_dump($image); ?>
+            <img src="<?php echo IMG_TARGET_FOLDER . $image->name; ?>" alt="" />
+        <?php endforeach; ?>
+    <?php else: ?>
+        Connectez-vous pour voir les images de tous les utilisateurs.
+    <?php endif; ?>
 </div>
