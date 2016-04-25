6db226b1435988f918e85aa06f4332af674e51e1
MeltedPenguin <diablo22007@gmail.com>
2016-04-25 17:07:06 +0200

    Sanitized some user input and distinct home page content

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,6 @@
 <div id="content">
     <?php if ($Auth->isLogged()): ?>
-        <?php foreach ($ImageManager->getImages() as $image): ?>
-            <?php var_dump($image); ?>
-            <img src="<?php echo IMG_TARGET_FOLDER . $image->name; ?>" alt="" />
-            <a href="?delete_image=<?php echo $image->id; ?>">Supprimer</a>
-        <?php endforeach; ?>
+        Bienvenue <b><?php echo $_SESSION['Auth']['login']; ?><b>
     <?php else: ?>
         Connectez-vous pour voir les images de tous les utilisateurs.
     <?php endif; ?>
