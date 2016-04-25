d19982f9211eb9a03d242680e38d77fabdb8b0c8
MeltedPenguin <diablo22007@gmail.com>
2016-04-25 14:47:35 +0200

    Database creation file

---
+++ bdd.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `images` (
+  `id`      int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `name`    varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `images`
+  ADD CONSTRAINT `image_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
